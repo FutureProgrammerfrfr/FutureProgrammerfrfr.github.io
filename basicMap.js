933ad11f74df1c50698352b1cc5bd70d37c04b02
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-11 14:12:00 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -4,7 +4,7 @@ console.log("JK JS FIle was loaded")
 // 35.217665 W, 80.31473 N -> 35.217665, -80.31473
 
 let map = L.map("map", {
-  center: [35.217665, -80.31473],
+  center: [35.2176665, -80.31473],
   zoom: 15
 });
 

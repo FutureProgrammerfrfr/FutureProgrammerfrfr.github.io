6754649172898e1baf4eac0b501e35c08098ff04
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-11 14:02:19 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,2 +1,13 @@
 console.log("Hello World!")
 console.log("JK JS FIle was loaded")
+
+// 35.217665 W, 80.31473 N -> 35.217665, -80.31473
+
+let map = L.map("map", {
+  center: [35.217665, -80.31473],
+  zoom: 8
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);

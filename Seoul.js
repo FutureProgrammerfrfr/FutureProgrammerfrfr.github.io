214c841838fcb5ff015d1f28ee61905e412ef5f4
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-16 14:26:54 -0400

    Update Seoul.js

--- Seoul.js
+++ Seoul.js
@@ -20,7 +20,7 @@ let marker = L.marker(
 
 // 37.551170, 126.988228 N Seoul Tower
 
-L.circle([37,551170, 126.988228], {
+L.circle([37.551170, 126.988228], {
         fillcolor: "orange",
         color: "white",
         radius: 1000

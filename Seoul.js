4191c0bfc22bdbf1fb19fc061c33475d58672ea6
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-16 13:49:19 -0400

    Update Seoul.js

--- Seoul.js
+++ Seoul.js
@@ -15,5 +15,5 @@ let marker = L.marker(
   [37.532600, 127.024612], {
     title: "South Korea, Seoul"
   }
-).bindPopup(`<h2>Hello from SEOUL</h2> <iframe width="560" height="315" src="https://www.youtube.com/embed/SRpMapyw6Aw?si=_IXXfMF_k3a2iVMl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
+).bindPopup(`<h2>Hello from SEOUL</h2> <iframe width="400" height="400" src="https://www.youtube.com/embed/40xZVEFVBuE?si=Q4HnfnCOSANJ0NVX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
   .addTo(map);

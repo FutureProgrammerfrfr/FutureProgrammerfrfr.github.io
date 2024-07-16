5697eb355a7d52796100852ee4fe7bf2a93c4f94
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-16 13:40:42 -0400

    Update Seoul.js

--- Seoul.js
+++ Seoul.js
@@ -10,3 +10,10 @@ let map = L.map("map", {
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+let marker = L.marker(
+  [37.532600, 127.024612], {
+    title: "South Korea, Seoul"
+  }
+).bindPopup(<h1>Hello from SEOUL</h1>)
+  .addTo(map);

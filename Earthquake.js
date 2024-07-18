7c26a1d20972d3823f275f39c34f706a7fbc6f9b
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-18 14:01:31 -0400

    Update Earthquake.js

--- Earthquake.js
+++ Earthquake.js
@@ -1,6 +1,17 @@
 console.log("Yes this works")
 
-let map 
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+d3.json(url).then(
+    function(data) 
+    {
+        console.log(data);
+    }
+);
+
+let map = L.map("map", {
+    zoom: 8 
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'

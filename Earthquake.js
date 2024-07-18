e3a311c68002141b00d36ac7eaeb5a681e871f20
FutureProgrammerfrfr <coolneil19@gmail.com>
2024-07-18 13:45:02 -0400

    Create Earthquake.js

---
+++ Earthquake.js
@@ -0,0 +1,7 @@
+console.log("Yes this works")
+
+let map 
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
